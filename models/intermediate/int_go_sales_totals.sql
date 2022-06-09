40a00c8bc8746b46cdbabd6e0f4acee1b89258ac
raghukovvuru <raghu@raghu.localdomain>
2022-06-09 11:19:00 +0530

    Intermediate models

---
+++ models/intermediate/int_go_sales_totals.sql
@@ -0,0 +1,19 @@
+with sales as(
+    select * from {{ref('stg_go_daily_sales')}}
+),
+products as(
+    select * from {{ref('stg_go_products')}}
+),
+calculated as(
+    select s.order_id,
+           s.product_id,
+           s.retailer_id,
+           s.quantity_sold,
+           p.unit_price,
+           (p.unit_price * s.quantity_sold) as sale_total
+    from sales as s 
+    left join products as p on 
+    s.product_id = p.product_id
+)
+
+select * from calculated
